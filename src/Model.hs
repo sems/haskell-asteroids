443ff02c4950b3a845a9b7c92053fdabb4e68c65
Sem <me@sems.dev>
2021-10-25 20:07:46 +0200

    Inital data-types

--- src/Model.hs
+++ src/Model.hs
@@ -10,9 +10,47 @@ nO_SECS_BETWEEN_CYCLES :: Float
 nO_SECS_BETWEEN_CYCLES = 5
 
 data GameState = GameState {
-                   infoToShow  :: InfoToShow
-                 , elapsedTime :: Float
-                 }
+  currentState :: State,
+  player1 :: Player,
+  player2 :: Player,
+  asteroids :: [Asteroid],
+  bullets :: [Bullet] 
+}
+
+Data Player = Player{
+  lives :: Int,
+  position :: (Float, Float),
+  direction :: (Float, Float),
+  time :: Float 
+}
+
+Data Bullet = Bullet {
+  position :: (Float, Float ),
+  direction :: (Float, Float) 
+}
+
+Data Asteroid = Asteroid {
+  position :: (Float, Float),
+  direction :: (Float, Float),
+  size :: Int,
+  speed :: Int 
+}
+
+Data ScoreEntry = ScoreEntry {
+  name :: String,
+  score :: Int,
+  mode :: GameMode 
+}
+
+Data State = Main
+  | Playing
+  | GameOver
+  | Pause
+  | Leaderboard
+  | Choose
+
+
+Data GameMode = Co-Op | SinglePlayer
 
 initialState :: GameState
 initialState = GameState ShowNothing 0
