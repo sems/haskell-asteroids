5041ba38a3e707223804b5e09608cd5125a7c65c
Sem <me@sems.dev>
2021-10-29 22:21:05 +0200

    Base movement function

--- src/Controller.hs
+++ src/Controller.hs
@@ -9,7 +9,7 @@ import System.Random
 import Data.Set
 import qualified Data.Set as S
 import Constants (screenWidth, screenHeigth)
-import Model (Asteroid(Asteroid))
+import Model (Asteroid(Asteroid), GameState (GameState))
 import Text.Printf (printf)
 
 -- | Handle one iteration of the game
@@ -39,6 +39,8 @@ stateFlow (EventKey (Char 'l') _ _ _) gstate@(GameState Main _ _ _ _ _) = gstate
 stateFlow (EventKey (Char 'm') _ _ _) gstate@(GameState Leaderboard _ _ _ _ _) = gstate {currentState = Main} -- back to main menu 
 stateFlow (EventKey (Char 'l') _ _ _) gstate@(GameState GameOver _ _ _ _ _) = initialState{currentState = Leaderboard}
 stateFlow (EventKey (Char 'm') _ _ _) gstate@(GameState GameOver _ _ _ _ _) = initialState
+-- movement
+stateFlow (EventKey (Char 'w') _ _ _) gstate@(GameState Playing p1 _ _ _ _) = movePlayer p1 UpDir gstate
 stateFlow _ gstate = gstate
 
 handleTime :: Float -> GameState -> GameState -- updates time for each player while in playing state if player is alive (when both players are alive their time are the same so the old time for player1 can be reused for player 2)
@@ -78,3 +80,14 @@ newAsteroid = do
 
 pureAsteroid :: Asteroid
 pureAsteroid = Asteroid (realToFrac screenWidth/2, realToFrac screenHeigth/2) (0,0) 2 1
+
+data MoveDirection = UpDir | DownDir | LeftDir | RightDir
+
+movePlayer :: Player -> MoveDirection -> GameState -> GameState
+movePlayer p@(Player liv post dir oldTime) d gstate = gstate{player1 = movePlayer' d p} 
+
+movePlayer' :: MoveDirection -> Player -> Player 
+movePlayer' UpDir p@(Player _ (x,y) _ _) = p{playerPos = (x,y+1)}
+movePlayer' DownDir p@(Player _ (x,y) _ _) = p{playerPos = (x,y-1)}
+movePlayer' LeftDir p@(Player _ (x,y) _ _) = p{playerPos = (x-1,y)}
+movePlayer' RightDir p@(Player _ (x,y) _ _) = p{playerPos = (x+1,y)}
