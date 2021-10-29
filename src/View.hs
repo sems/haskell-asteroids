c1a60aac34a63a57bfdd01221ac1f188bcb05d66
Sem <me@sems.dev>
2021-10-29 17:20:40 +0200

    Cleanup

--- src/View.hs
+++ src/View.hs
@@ -18,25 +18,25 @@ viewPure gstate = case currentState gstate of -- temp indications for states
   Choose      -> color green (text "choose")
 
 drawGameOver :: GameState -> [Picture]
-drawGameOver gstate@(GameState _ (Player _ _ _ time1) (Player _ _ _ time2) _ _) = 
-  color green (text "Gameover") : (translate 0 (-100) $ color green (text $ show score) ): (translate 0 (-200) $ color green (text $ show mode)) : []
+drawGameOver gstate@(GameState _ (Player _ _ _ time1) (Player _ _ _ time2) _ _ _) =
+  [color green (text "Gameover"), translate 0 (-100) $ color green (text $ show score), translate 0 (-200) $ color green (text $ show mode)]
    where score = round (time1 + time2)
          mode  | time2 == 0 = SinglePlayer
                | otherwise = Coop
 
-drawPlaying :: GameState -> [Picture] 
-drawPlaying gstate@(GameState _ p1 p2 _ _)  =  drawScore (-490,150) p1 : drawScore (0,150) p2 : drawPlayers gstate
+drawPlaying :: GameState -> [Picture]
+drawPlaying gstate@(GameState _ p1 p2 _ _ _)  =  drawScore (-490,150) p1 : drawScore (0,150) p2 : drawPlayers gstate
   where drawScore _ (Player _ _ _ 0) = blank
-        drawScore (x,y) (Player lives _ _ time) = translate x y $ color white $ text ("l:" ++ show lives ++ " s:" ++ show (round(time)))
+        drawScore (x,y) (Player lives _ _ time) = translate x y $ color white $ text ("l:" ++ show lives ++ " s:" ++ show (round time))
 
 
 drawPlayers :: GameState -> [Picture]
-drawPlayers (GameState _ (Player lives1 pos1 _ _) (Player lives2 pos2 _ _) _ _) = [drawPlayer pos1 blue lives1 , drawPlayer pos2 yellow lives2]
+drawPlayers (GameState _ (Player lives1 pos1 _ _) (Player lives2 pos2 _ _) _ _ _) = [drawPlayer pos1 blue lives1 , drawPlayer pos2 yellow lives2]
   where drawPlayer _ _ 0 = blank
         drawPlayer (x,y) col _ = translate x y $ color col $ polygon [(0,0),(10,30),(20,0)]
 
- 
-  
+
+
 
 
 
